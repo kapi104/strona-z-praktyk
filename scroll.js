fc112828d185b1ff02716838cbf3222f4871f969
kapi104 <skorupa7979@gmail.com>
2022-10-13 20:42:12 +0200

    added scrolling feature to buttons

--- scroll.js
+++ scroll.js
@@ -3,11 +3,18 @@ const next = document.querySelector('.next')
 const prev = document.querySelector('.previous')
 let currentPage = 0;
 
-
-
-function scrollTo(num) {
-    pages[num].scrollIntoView({ behavior: 'smooth' }, true);
+function scrollTo() {
+    pages[currentPage].scrollIntoView({ behavior: 'smooth' }, true);
     return;
 }
 
-next.addEventListener('click', scrollTo)
+next.addEventListener('click', () => {
+    if (currentPage != 4)
+    currentPage += 1;
+    scrollTo()
+}, false);
+prev.addEventListener('click', () => {
+    if (currentPage != 0)
+    currentPage += -1;
+    scrollTo()
+}, false);
