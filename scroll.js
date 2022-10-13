222f7de10e42dda6e301a4e5cff46e2421adff78
kapi104 <skorupa7979@gmail.com>
2022-10-13 20:11:48 +0200

    changed scroll mode, added scroll buttons

--- scroll.js
+++ scroll.js
@@ -1,37 +1,13 @@
 const pages = document.querySelectorAll('.page');
+const next = document.querySelector('.next')
+const prev = document.querySelector('.previous')
 let currentPage = 0;
-let y = 0;
-let date = Date.now();
 
-setInterval(function () {
-    y = scrollY;
-}, 200)
+
 
 function scrollTo(num) {
     pages[num].scrollIntoView({ behavior: 'smooth' }, true);
-    
     return;
 }
 
-function check() {
-    if (scrollY > y) {
-        currentPage += 1;
-        return currentPage;
-    } else if (scrollY < y) {
-        currentPage -= 1;
-    } else {
-        return currentPage;
-    }
-}
-
-function wait(fn, delay) {
-    return function () {
-        if ((date + delay - Date.now()) < 0) {
-            fn(check());
-            date = Date.now();
-            
-        }
-    }
-}
-
-document.addEventListener('scroll', wait(scrollTo, 1000), {passive: true});
+next.addEventListener('click', scrollTo)
