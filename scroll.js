ae150900a5776436577547742b29bbc5f7f0dc5d
kapi104 <skorupa7979@gmail.com>
2022-10-13 22:37:07 +0200

    finished scroll script

--- scroll.js
+++ scroll.js
@@ -3,18 +3,95 @@ const next = document.querySelector('.next')
 const prev = document.querySelector('.previous')
 let currentPage = 0;
 
+
 function scrollTo() {
     pages[currentPage].scrollIntoView({ behavior: 'smooth' }, true);
     return;
 }
 
+function getBackgroundColor() {
+    if (currentPage == 0) {
+        return color = 'rgb(73, 73, 73)';
+    } else {
+        let tab = document.querySelector(`.tab${currentPage}`)
+        let color = window.getComputedStyle(tab).backgroundColor;
+        return color;
+    }
+}
+
+next.addEventListener('mouseenter', () => {
+    if (currentPage != 4) {
+        next.style.backgroundColor = getBackgroundColor();
+    }
+
+})
+next.addEventListener('mouseleave', () => {
+    next.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+})
+
+prev.addEventListener('mouseenter', () => {
+    if (currentPage != 0) {
+        prev.style.backgroundColor = getBackgroundColor();
+    }
+
+})
+prev.addEventListener('mouseleave', () => {
+    prev.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+})
+
+function toggleOpacity() {
+    if (currentPage != 0) {
+        prev.classList.remove('transparent');
+    } else {
+        prev.classList.add('transparent');
+    }
+    if (currentPage != 4) {
+        next.classList.remove('transparent');
+    } else {
+        next.classList.add('transparent');
+    }
+}
+
+function dissapear() {
+    prev.classList.add('transparent');
+    next.classList.add('transparent');
+    setTimeout(function() {
+        prev.classList.add('dissapear');
+        next.classList.add('dissapear');
+    },500)
+}
+
+function appear() {
+    if (currentPage != 0) {
+        prev.classList.remove('dissapear');
+        prev.classList.remove('transparent');
+    }
+    if (currentPage != 4) {
+        next.classList.remove('dissapear');
+        next.classList.remove('transparent');
+    }
+}
+
 next.addEventListener('click', () => {
     if (currentPage != 4)
-    currentPage += 1;
-    scrollTo()
+        currentPage += 1;
+        dissapear()
+        setTimeout(() => {
+            scrollTo()
+        }, 450)
+        setTimeout(() => {
+            appear()
+        }, 900)
 }, false);
+
 prev.addEventListener('click', () => {
     if (currentPage != 0)
-    currentPage += -1;
-    scrollTo()
-}, false);
+        currentPage += -1;
+        dissapear()
+        setTimeout(function () {
+            scrollTo()
+        }, 450)
+        setTimeout(function () {
+            appear()
+    }, 900)
+}, false)
