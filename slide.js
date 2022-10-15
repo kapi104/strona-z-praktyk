64e7d19ec656ede73a3e60b1ec7b7c192dbe6a2e
kapi104 <skorupa7979@gmail.com>
2022-10-15 22:27:55 +0200

    created enlarge funcction

--- slide.js
+++ slide.js
@@ -1,43 +1,91 @@
-const slidesLeft = document.querySelectorAll('.tabl');
-const slidesRight = document.querySelectorAll('.tabr');
+const arrowl = document.querySelectorAll('.arrowl');
+const arrowr = document.querySelectorAll('.arrowr');
+const tabs = document.querySelectorAll('.tab')
 let styleElement = document.head.appendChild(document.createElement("style"));
+let bcgColor;
 
-slidesLeft.forEach((slide) => {
+// on hover slide
+
+arrowl.forEach((slide) => {
     slide.addEventListener('mouseenter', () => {
-        let bcgColor = getBackgroundColor(currentPage + 1);
-        styleElement.innerHTML = `.tabl::before {transform: translateX(100%);
-        background-color: ${bcgColor};
-    }`;
+        if (!slide.classList.contains('enlarge')) {
+            bcgColor = getBackgroundColor(currentPage + 1);
+            styleElement.innerHTML = `.tabl::before {transform: translateX(100%);
+            background-color: ${bcgColor};}`;
+        }
     });
-    
+
     slide.addEventListener('mouseleave', () => {
-        let bcgColor = getBackgroundColor(currentPage + 1);
-        styleElement.innerHTML = `.tabl::before {transform: translateX(0);
-        background-color: ${bcgColor};
-    }`;
+        slideOutLeft()
     });
 })
 
-slidesRight.forEach((slide) => {
+arrowr.forEach((slide) => {
     slide.addEventListener('mouseenter', () => {
-        if (slide.classList.contains('tab4')) {
-            bcgColor = getBackgroundColor('1');
-        } else {
-            bcgColor = getBackgroundColor(currentPage + 1);
+        if (!slide.classList.contains('enlarge')) {
+            if (currentPage == 4) {
+                bcgColor = getBackgroundColor('1');
+            } else {
+                bcgColor = getBackgroundColor(currentPage + 1);
+            }
+            styleElement.innerHTML = `.tabr::after {transform: translateX(-100%);
+            background-color: ${bcgColor};}`;
         }
-        styleElement.innerHTML = `.tabr::after {transform: translateX(-100%);
-        background-color: ${bcgColor};
-    }`;
     });
 
     slide.addEventListener('mouseleave', () => {
-        if (slide.classList.contains('tab4')) {
-            bcgColor = getBackgroundColor('1');
-        } else {
-            bcgColor = getBackgroundColor(currentPage + 1);
-        }
-        styleElement.innerHTML = `.tabr::after {background-color: ${bcgColor};
+        slideOutRight(slide);
+    });
+
+})
+
+arrowl.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+        slideOutLeft()
+        setTimeout(() => {
+            if (!tabs[currentPage - 1].classList.contains('enlarge')) {
+                tabs[currentPage - 1].classList.add('enlarge');
+                arrow.classList.add('reverse');
+            } else {
+                tabs[currentPage - 1].classList.remove('enlarge');
+                arrow.classList.remove('reverse');
+            }
+        }, 200);
+    })
+})
+
+arrowr.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+        slideOutRight();
+        setTimeout(() => {
+            if (!tabs[currentPage - 1].classList.contains('enlarge')) {
+                tabs[currentPage - 1].classList.add('enlarge');
+                arrow.classList.add('reverse');
+            }else {
+                tabs[currentPage - 1].classList.remove('enlarge');
+                arrow.classList.remove('reverse');
+            }
+        }, 200);
+    })
+})
+
+function slideOutRight() {
+    if (currentPage == 4) {
+        bcgColor = getBackgroundColor('1');
+    } else {
+        bcgColor = getBackgroundColor(currentPage + 1);
+    }
+    styleElement.innerHTML = `.tabr::after {background-color: ${bcgColor};
         transform: translateX(0);
         }`;
-    });
-})
+    return
+}
+
+function slideOutLeft() {
+    bcgColor = getBackgroundColor(currentPage + 1);
+    styleElement.innerHTML = `.tabl::before {transform: translateX(0);
+        background-color: ${bcgColor};
+    }`;
+    return
+}
+
